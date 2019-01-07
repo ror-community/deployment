8bb6eb38156934b6251c2921cc615abc1348bf06
Martin Fenner <martin.fenner@datacite.org>
2019-01-07 07:38:54 +0100

    comment out public dns

--- ror/dns/main.tf
+++ ror/dns/main.tf
@@ -1,23 +1,23 @@
-resource "aws_route53_zone" "public" {
-    name = "ror.org"
+// resource "aws_route53_zone" "public" {
+//     name = "ror.org"
 
-    tags {
-        Environment = "public"
-    }
-}
+//     tags {
+//         Environment = "public"
+//     }
+// }
 
-resource "aws_route53_record" "public-ns" {
-    zone_id = "${aws_route53_zone.public.zone_id}"
-    name = "ror.org"
-    type = "NS"
-    ttl = "300"
-    records = [
-        "${aws_route53_zone.public.name_servers.0}",
-        "${aws_route53_zone.public.name_servers.1}",
-        "${aws_route53_zone.public.name_servers.2}",
-        "${aws_route53_zone.public.name_servers.3}"
-    ]
-}
+// resource "aws_route53_record" "public-ns" {
+//     zone_id = "${aws_route53_zone.public.zone_id}"
+//     name = "ror.org"
+//     type = "NS"
+//     ttl = "300"
+//     records = [
+//         "${aws_route53_zone.public.name_servers.0}",
+//         "${aws_route53_zone.public.name_servers.1}",
+//         "${aws_route53_zone.public.name_servers.2}",
+//         "${aws_route53_zone.public.name_servers.3}"
+//     ]
+// }
 
 resource "aws_route53_zone" "internal" {
     name = "ror.org"
