cc74c6dc1aec4e0c2c14821cfc84f2cdda22ba49
Martin Fenner <martin.fenner@datacite.org>
2019-01-07 08:45:24 +0100

    comment out iam_account module

--- ror/iam/main.tf
+++ ror/iam/main.tf
@@ -1,9 +1,9 @@
-module "iam_account" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-account"
+// module "iam_account" {
+//   source = "terraform-aws-modules/iam/aws//modules/iam-account"
 
-  account_alias = "${var.account_alias}"
+//   account_alias = "${var.account_alias}"
 
-  minimum_password_length = "${var.minimum_password_length}"
-  require_numbers         = "${var.require_numbers}"
-  require_symbols         = "${var.require_symbols}"
-}
+//   minimum_password_length = "${var.minimum_password_length}"
+//   require_numbers         = "${var.require_numbers}"
+//   require_symbols         = "${var.require_symbols}"
+// }
