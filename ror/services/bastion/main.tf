1cb1f7c0e4abca94b9581ca2bc8b6b48efd57edf
Martin Fenner <martin.fenner@datacite.org>
2019-01-10 00:05:52 +0100

    use data attribute

--- ror/services/bastion/main.tf
+++ ror/services/bastion/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "bastion" {
 resource "aws_instance" "test" {
     ami = "${var.ami["eu-west-1"]}"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.private_security_group.id}"]
+    vpc_security_group_ids = ["${data.aws_security_group.private_security_group.id}"]
     subnet_id = "${data.aws_subnet.private_subnet.id}"
     key_name = "${var.key_name}"
     tags {
