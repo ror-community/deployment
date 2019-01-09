4056afc697429207b65455a362427cbe8ab4c263
Martin Fenner <martin.fenner@datacite.org>
2019-01-09 19:27:38 +0100

    remove eks cluster

--- ror/eks/main.tf
+++
@@ -1,7 +0,0 @@
-module "eks" {
-  source                = "terraform-aws-modules/eks/aws"
-  
-  cluster_name          = "${var.cluster_name}"
-  subnets               = "${var.private_subnet_ids}"
-  vpc_id                = "${var.vpc_id}"
-}
