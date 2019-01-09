be47b217941f56e94faf880bedb5655e11fafb1b
Martin Fenner <martin.fenner@datacite.org>
2019-01-09 21:15:31 +0100

    security groups for ecs

--- ror/vpc/main.tf
+++ ror/vpc/main.tf
@@ -11,19 +11,61 @@ module "vpc" {
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  vpc_tags = "${
-    map(
-     "kubernetes.io/cluster/${var.cluster_name}", "shared",
-    )
-  }"
-
-  private_subnet_tags = "${
-    map(
-     "kubernetes.io/cluster/${var.cluster_name}", "shared",
-    )
-  }"
-
   tags = {
     Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "load-balancer-sg"
+  description = "Allow all inbound traffic to http and https"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port    = 80
+    to_port      = 80
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port    = 443
+    to_port      = 443
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "load-balancer-sg"
+  }
+}
+
+resource "aws_security_group" "ecs_service_sg" {
+  name        = "fargate-ecs-service-sg"
+  description = "Allow all inbound traffic to service port"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.lb_sg.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "fargate-ecs-service-sg"
   }
 }
