9ae0e8565ab744b114804d00e0669475ea304461
Martin Fenner <martin.fenner@datacite.org>
2019-01-17 17:00:01 +0100

    use other origin

--- ror/vpc/cloudfront.tf
+++ ror/vpc/cloudfront.tf
@@ -1,20 +1,20 @@
 resource "aws_cloudfront_distribution" "site" {
-  // origin {
-  //   domain_name = "${data.aws_s3_bucket.ror-org-s3.website_endpoint}"
-  //   origin_id = "${data.aws_s3_bucket.ror-org-s3.bucket_domain_name}"
-
-  //   s3_origin_config {
-  //     origin_access_identity = "${aws_cloudfront_origin_access_identity.ror_org.cloudfront_access_identity_path}"
-  //   }
-  // }
   origin {
-    domain_name = "${data.aws_s3_bucket.search.bucket_domain_name}"
-    origin_id   = "search.ror.org"
+    domain_name = "${data.aws_s3_bucket.ror-org-s3.website_endpoint}"
+    origin_id = "${data.aws_s3_bucket.ror-org-s3.bucket_domain_name}"
 
-    // s3_origin_config {
-    //   origin_access_identity = "${aws_cloudfront_origin_access_identity.search_ror_org.cloudfront_access_identity_path}"
-    // }
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.ror_org.cloudfront_access_identity_path}"
+    }
   }
+  // origin {
+  //   domain_name = "${data.aws_s3_bucket.search.bucket_domain_name}"
+  //   origin_id   = "search.ror.org"
+
+  //   // s3_origin_config {
+  //   //   origin_access_identity = "${aws_cloudfront_origin_access_identity.search_ror_org.cloudfront_access_identity_path}"
+  //   // }
+  // }
 
   tags {
     site        = "ror"
