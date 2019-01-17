1a055c92491be2f8fbdd2b4fe4a8806c8825b109
Geoffrey Bilder <gbilder@crossref.org>
2019-01-17 20:36:13 +0100

    add reconcile service

---
+++ ror/services/reconcile/var.tf
@@ -0,0 +1,33 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "ttl" {
+  default = "300"
+}
+
+variable "vpc_id" {}
+
+variable "private_subnet_ids" {
+  type = "list"
+}
+
+variable "private_security_group_id" {}
+
+# variable "elastic_search" {
+#   default = "http://elasticsearch.ror.org:80"
+# }
+
+# variable "es_name" {
+#   default = "es"
+# }
+
+variable "ror-reconcile_tags" {
+  type = "map"
+}
+
+variable "public_key" {}
+variable "bugsnag_key" {}
