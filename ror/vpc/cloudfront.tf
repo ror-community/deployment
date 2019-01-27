b7f36e50e0bbcb7727287f5246fe5fff4f66e90e
Martin Fenner <martin.fenner@datacite.org>
2019-01-27 10:43:24 +0100

    add cloudfront for api

--- ror/vpc/cloudfront.tf
+++ ror/vpc/cloudfront.tf
@@ -185,3 +185,68 @@ resource "aws_route53_record" "apex" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_cloudfront_distribution" "api" {
+  origin {
+    domain_name = "${aws_lb.alb.domain_name}"
+    origin_id = "${aws_lb.alb.id}"
+  }
+
+  tags {
+    site        = "ror-api"
+    environment = "production"
+  }
+
+  aliases             = ["api.ror.org"]
+  enabled             = "true"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${aws_lb.alb.id}"
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    # This says to redirect http to https
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = "true"
+    min_ttl                = 0
+
+    # default cache time in seconds.  This is 1 day, meaning CloudFront will only
+    # look at your S3 bucket for changes once per day.
+    default_ttl            = 86400
+    max_ttl                = 2592000
+
+    // lambda_function_association {
+    //   event_type   = "origin-request"
+    //   lambda_arn   = "${aws_lambda_function.index-page.qualified_arn}"
+    //   include_body = false
+    // }
+  }
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${data.aws_s3_bucket.logs.bucket_domain_name}"
+
+    prefix = "cf-api/"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "${data.aws_acm_certificate.cloudfront.arn}"
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1"
+  }
+}
