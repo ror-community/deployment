5e3ea1177e8bdadabda09ccba862f583bb13655c
Martin Fenner <martin.fenner@datacite.org>
2019-05-28 22:20:18 +0200

    stay with terraform 0.11 for now

--- ror/data-storage/elasticsearch/input.tf
+++ ror/data-storage/elasticsearch/input.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-  version    = "~> 2.7"
+  version    = "~> 1.6"
 }
 
 data "aws_security_group" "private_security_group" {
