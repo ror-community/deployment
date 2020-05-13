1e595effb9da33f06a6ee0654c5ee634f9d82c91
Martin Fenner <martin.fenner@datacite.org>
2020-05-13 22:09:27 +0200

    terraform 0.12 #3

--- ror/services/bastion/input.tf
+++ ror/services/bastion/input.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
-  version    = "~> 1.6"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+  version    = "~> 2.7"
 }
 
 data "aws_route53_zone" "public" {
@@ -13,19 +13,19 @@ data "aws_route53_zone" "internal" {
   private_zone = true
 }
 data "aws_subnet" "public_subnet" {
-  id = "${var.public_subnet_id}"
+  id = var.public_subnet_id
 }
 data "aws_subnet" "private_subnet" {
-  id = "${var.private_subnet_id}"
+  id = var.private_subnet_id
 }
 data "aws_security_group" "private_security_group" {
-  id = "${var.private_security_group_id}"
+  id = var.private_security_group_id
 }
 data "template_file" "bastion-user-data-cfg" {
-  template = "${file("user_data.cfg")}"
+  template = file("user_data.cfg")
 
   vars {
-    hostname     = "${var.hostname}"
-    fqdn         = "${var.hostname}.ror.org"
+    hostname     = var.hostname
+    fqdn         = var.hostname}.ror.org"
   }
 }
