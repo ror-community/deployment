9bad422ecb8a8671521123edb7782e388396db1f
Martin Fenner <martin.fenner@datacite.org>
2020-12-01 16:39:07 +0000

    remove obsolete internal dns

--- ror/vpc/dns.tf
+++ ror/vpc/dns.tf
@@ -10,30 +10,30 @@ resource "aws_route53_zone" "public" {
     }
 }
 
-resource "aws_route53_zone" "internal" {
-    name = "ror.org"
+// resource "aws_route53_zone" "internal" {
+//     name = "ror.org"
     
-    vpc {
-        vpc_id = module.vpc.vpc_id
-    }
+//     vpc {
+//         vpc_id = module.vpc.vpc_id
+//     }
 
-    tags = {
-        Environment = "internal"
-    }
-}
+//     tags = {
+//         Environment = "internal"
+//     }
+// }
 
-resource "aws_route53_record" "internal-ns" {
-    zone_id = aws_route53_zone.internal.zone_id
-    name = "ror.org"
-    type = "NS"
-    ttl = "30"
-    records = [
-        aws_route53_zone.internal.name_servers.0,
-        aws_route53_zone.internal.name_servers.1,
-        aws_route53_zone.internal.name_servers.2,
-        aws_route53_zone.internal.name_servers.3
-    ]
-}
+// resource "aws_route53_record" "internal-ns" {
+//     zone_id = aws_route53_zone.internal.zone_id
+//     name = "ror.org"
+//     type = "NS"
+//     ttl = "30"
+//     records = [
+//         aws_route53_zone.internal.name_servers.0,
+//         aws_route53_zone.internal.name_servers.1,
+//         aws_route53_zone.internal.name_servers.2,
+//         aws_route53_zone.internal.name_servers.3
+//     ]
+// }
 
 // resource "aws_service_discovery_private_dns_namespace" "internal" {
 //   name = "ror.org"
