7018f759a9caffb387090e5455dcabe0486da55a
Martin Fenner <martin.fenner@datacite.org>
2020-12-01 15:42:46 +0000

    remove obsolete security groups

--- ror/vpc/main.tf
+++ ror/vpc/main.tf
@@ -19,56 +19,56 @@ module "vpc" {
   }
 }
 
-resource "aws_security_group" "lb_sg" {
-  name        = "load-balancer-sg"
-  description = "Allow all inbound traffic to http and https"
-  vpc_id      = module.vpc.vpc_id
+// resource "aws_security_group" "lb_sg" {
+//   name        = "load-balancer-sg"
+//   description = "Allow all inbound traffic to http and https"
+//   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port    = 80
-    to_port      = 80
-    protocol     = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
-  }
+//   ingress {
+//     from_port    = 80
+//     to_port      = 80
+//     protocol     = "tcp"
+//     cidr_blocks  = ["0.0.0.0/0"]
+//   }
 
-  ingress {
-    from_port    = 443
-    to_port      = 443
-    protocol     = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
-  }
+//   ingress {
+//     from_port    = 443
+//     to_port      = 443
+//     protocol     = "tcp"
+//     cidr_blocks  = ["0.0.0.0/0"]
+//   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "load-balancer-sg"
-  }
-}
+//   egress {
+//     from_port   = 0
+//     to_port     = 0
+//     protocol    = "-1"
+//     cidr_blocks = ["0.0.0.0/0"]
+//   }
+//   tags = {
+//     Name = "load-balancer-sg"
+//   }
+// }
 
-resource "aws_security_group" "ecs_service_sg" {
-  name        = "fargate-ecs-service-sg"
-  description = "Allow all inbound traffic to service port"
-  vpc_id      = module.vpc.vpc_id
+// resource "aws_security_group" "ecs_service_sg" {
+//   name        = "fargate-ecs-service-sg"
+//   description = "Allow all inbound traffic to service port"
+//   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [var.vpc_cidr]
-  }
+//   ingress {
+//     from_port = 0
+//     to_port = 0
+//     protocol = "-1"
+//     cidr_blocks = [var.vpc_cidr]
+//   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+//   egress {
+//     from_port   = 0
+//     to_port     = 0
+//     protocol    = "-1"
+//     cidr_blocks = ["0.0.0.0/0"]
+//   }
 
-  tags = {
-    Name = "fargate-ecs-service-sg"
-  }
-}
+//   tags = {
+//     Name = "fargate-ecs-service-sg"
+//   }
+// }
