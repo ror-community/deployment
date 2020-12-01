ecb5e74d7a2dacd0ea3c4d24054495286dddaa41
Martin Fenner <martin.fenner@datacite.org>
2020-12-01 07:00:56 +0000

    retire old es cluster

--- ror/data-storage/elasticsearch/main.tf
+++ ror/data-storage/elasticsearch/main.tf
@@ -2,49 +2,49 @@ resource "aws_iam_service_linked_role" "es" {
   aws_service_name = "es.amazonaws.com"
 }
 
-resource "aws_elasticsearch_domain" "ror" {
-  domain_name           = "elasticsearch-ror"
-  elasticsearch_version = "6.3"
+// resource "aws_elasticsearch_domain" "ror" {
+//   domain_name           = "elasticsearch-ror"
+//   elasticsearch_version = "6.3"
 
-  cluster_config {
-    instance_type = "m4.large.elasticsearch"
-    instance_count = 1
-  }
+//   cluster_config {
+//     instance_type = "m4.large.elasticsearch"
+//     instance_count = 1
+//   }
 
-  advanced_options = {
-    "rest.action.multi.allow_explicit_index" = "true"
-  }
+//   advanced_options = {
+//     "rest.action.multi.allow_explicit_index" = "true"
+//   }
 
-  snapshot_options {
-    automated_snapshot_start_hour = 23
-  }
+//   snapshot_options {
+//     automated_snapshot_start_hour = 23
+//   }
 
-  ebs_options{
-      ebs_enabled = true
-      volume_type = "gp2"
-      volume_size = 100
-  }
+//   ebs_options{
+//       ebs_enabled = true
+//       volume_type = "gp2"
+//       volume_size = 100
+//   }
 
-  vpc_options {
-    security_group_ids = [data.aws_security_group.private_security_group.id]
-    subnet_ids = [data.aws_subnet.private_subnet.id]
-  }
+//   vpc_options {
+//     security_group_ids = [data.aws_security_group.private_security_group.id]
+//     subnet_ids = [data.aws_subnet.private_subnet.id]
+//   }
 
-  tags = {
-    Domain = "elasticsearch-ror"
-  }
-}
+//   tags = {
+//     Domain = "elasticsearch-ror"
+//   }
+// }
 
-resource "aws_elasticsearch_domain_policy" "ror" {
-  domain_name = aws_elasticsearch_domain.ror.domain_name
+// resource "aws_elasticsearch_domain_policy" "ror" {
+//   domain_name = aws_elasticsearch_domain.ror.domain_name
 
-  access_policies = file("elasticsearch_policy.json")
-}
+//   access_policies = file("elasticsearch_policy.json")
+// }
 
-resource "aws_route53_record" "elasticsearch-ror" {
-   zone_id = data.aws_route53_zone.internal.zone_id
-   name = "elasticsearch.ror.org"
-   type = "CNAME"
-   ttl = var.ttl
-   records = [aws_elasticsearch_domain.ror.endpoint]
-}
+// resource "aws_route53_record" "elasticsearch-ror" {
+//    zone_id = data.aws_route53_zone.internal.zone_id
+//    name = "elasticsearch.ror.org"
+//    type = "CNAME"
+//    ttl = var.ttl
+//    records = [aws_elasticsearch_domain.ror.endpoint]
+// }
